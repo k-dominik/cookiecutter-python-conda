92d09bdc47b2977e7b403149abe8a342c7dee632
k-dominik <k-dominik@users.noreply.github.com>
2018-12-14 18:44:11 +0100

    added empty config files, plus removal hook

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,30 @@
+"""Clean-up, remove optional unused stuff"""
+import os
+
+
+def remove_black():
+    os.remove(".pre-commit")
+
+
+def remove_ci():
+    os.remove(".travis.yml")
+
+
+def remove_versioning():
+    os.remove(".bumpversion.cfg")
+
+
+def main():
+    print('removing unnecessary files...')
+    if "{{ cookiecutter.use_black }}".lower() == 'n':
+        remove_black()
+    if "{{ cookiecutter.use_ci }}".lower() == 'n':
+        remove_ci()
+    if "{{ cookiecutter.use_bumpversion }}".lower() == 'n':
+        remove_black()
+
+    print("project initialized!")
+
+
+if __name__ == '__main__':
+    main()
